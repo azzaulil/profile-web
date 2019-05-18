dc2bbb4ae67d2c8b76934531ace7ab57dd7314b5
spindycell <40053870+spindyzel@users.noreply.github.com>
2019-05-18 08:13:02 +0700

    tambah isi tiap page

--- src/page/Services.js
+++ src/page/Services.js
@@ -5,7 +5,91 @@ class Services extends Component {
     render() { 
         return ( 
             <React.Fragment>
-                Services
+                <div className="tab-pane active pt-page-rotateCarouselTopIn">
+                    <div className="service-area">
+                        <div className="row">
+                            <div className="col-md-4 col-sm-6 col-xs-12 text-center acurate">
+                                <div className="single-service">
+                                    <div className="service-icon">
+                                        <i className="flaticon-blog"></i>
+                                    </div>
+                                    <div className="setvice-content">
+                                        <h3>Creative Design</h3>
+                                        <p>ed ut perspiciatis unde omnis
+                                            iste natuserror sit voluptatem accusantium
+                                            doloremque laudantium, totam</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-xs-12 text-center acurate">
+                                <div className="single-service">
+                                    <div className="service-icon">
+                                        <i className="flaticon-agreement"></i>
+                                    </div>
+                                    <div className="setvice-content">
+                                        <h3>Web Design</h3>
+                                        <p>ed ut perspiciatis unde omnis
+                                            iste natuserror sit voluptatem accusantium
+                                            doloremque laudantium, totam</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-xs-12 text-center acurate">
+                                <div className="single-service">
+                                    <div className="service-icon">
+                                        <i className="flaticon-books-stack-of-three"></i>
+                                    </div>
+                                    <div className="setvice-content">
+                                        <h3>Interface Design</h3>
+                                        <p>ed ut perspiciatis unde omnis
+                                            iste natuserror sit voluptatem accusantium
+                                            doloremque laudantium, totam</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-xs-12 text-center acurate">
+                                <div className="single-service mb-0">
+                                    <div className="service-icon">
+                                        <i className="flaticon-edit-tools"></i>
+                                    </div>
+                                    <div className="setvice-content">
+                                        <h3>App UI Design</h3>
+                                        <p>ed ut perspiciatis unde omnis
+                                            iste natuserror sit voluptatem accusantium
+                                            doloremque laudantium, totam</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-xs-12 text-center acurate">
+                                <div className="single-service mb-0">
+                                    <div className="service-icon">
+                                        <i className="flaticon-analytics"></i>
+                                    </div>
+                                    <div className="setvice-content">
+                                        <h3>Print Production</h3>
+                                        <p>ed ut perspiciatis unde omnis
+                                            iste natuserror sit voluptatem accusantium
+                                            doloremque laudantium, totam</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-xs-12 text-center acurate">
+                                <div className="single-service mb-0">
+                                    <div className="service-icon">
+                                        <i className="flaticon-computer"></i>
+                                    </div>
+                                    <div className="setvice-content">
+                                        <h3>UX Design</h3>
+                                        <p>ed ut perspiciatis unde omnis
+                                            iste natuserror sit voluptatem accusantium
+                                            doloremque laudantium, totam</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>  
             </React.Fragment>
         );
     }
