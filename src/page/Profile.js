605753a4bf8a036d2f29434843504b097e7ec17c
spindycell <40053870+spindyzel@users.noreply.github.com>
2019-05-18 07:45:47 +0700

    navigation pake router dan buat navbar tinggal isi konten

---
+++ src/page/Profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+                Profile
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Profile;
---
+++ src/page/Resume.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+                Resume
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Resume;
